a701663ffb9d920e06b744b5ae76fc348f2a5593
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2023-03-29 23:49:27 -0600

    second commit

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-import bodyParser from "body-parser";
-import cors from 'cors';
 var app = express();
 //get PORT from the server
 //obtener el PUERTO del server donde hosteamos
 const PORT = process.env.PORT;
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-})); 
-
-//global
-//que si no da error
-const corsConfig = {
-    credentials: true,
-    origin: true,
-};
-app.use(cors(corsConfig));
 //end of global
 //test
 app.get("/", async (req, res, next) => {
