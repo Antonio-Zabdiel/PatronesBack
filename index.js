1272a7c655f6bbc8c13db69e626313167959005b
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2023-03-29 23:50:03 -0600

    3er commit

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
+const express = require("express");
+
 var app = express();
-//get PORT from the server
-//obtener el PUERTO del server donde hosteamos
+
 const PORT = process.env.PORT;
 
+//global
+app.all("", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+app.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 //end of global
+
 //test
 app.get("/", async (req, res, next) => {
-    res.json({
-        msg:"welcome >:3"
-    });
-});
+  res.json({msg:"welcome :3"});});
 
 //run this sheet
-//que se ejecute la cosa esta
 app.listen(PORT, () => {
-    console.log(Server listening on ${PORT});
+  console.log(Server listening on ${PORT});
 });
